0641bd49f8d13750e0538562b6c83f97c7f552fc
SamirFarhat17 <samirfarhat17@gmail.com>
2021-03-14 19:54:52 +0000

    Added users class

--- src/User.java
+++ src/User.java
@@ -8,16 +8,16 @@ public class User {
     public String userID;
     public ArrayList<Vault> vaults;
     public HashMap<String, Double> collaterals;
-    public double basketHoldings;
+    public double bsktHoldings;
     public HashMap<String,Double> feesOwed;
 
     // Constructor
     public User(String userID, ArrayList<Vault> vaults, HashMap<String,Double> collaterals,
-                double basketHoldings, HashMap<String,Double> feesOwed) {
+                double bsktHoldings, HashMap<String,Double> feesOwed) {
         this.userID = userID;
         this.vaults = vaults;
         this.collaterals = collaterals;
-        this.basketHoldings = basketHoldings;
+        this.bsktHoldings = bsktHoldings;
         this.feesOwed = feesOwed;
     }
 
@@ -25,12 +25,40 @@ public class User {
     public String getUserID() { return this.userID; }
 
     public ArrayList<Vault> getVaults() { return this.vaults; }
-    public ArrayList<Vault> setVaults(ArrayList<Vault> vaults) { this.vaults = vaults; }
+    public void setVaults(ArrayList<Vault> vaults) { this.vaults = vaults; }
+
     public ArrayList<Vault> appendVault(Vault v) {
         this.vaults.add(v);
         return this.vaults;
     }
 
-    
+    public HashMap<String,Double> getCollaterals() { return this.collaterals; }
+    public void setCollaterals(HashMap<String,Double> collaterals) { this.collaterals = collaterals; }
+    // Add collaterals without duplicates
+    public HashMap<String,Double> addCollaterals(String collateralType, double amount) {
+        if(!this.collaterals.containsKey(collateralType)) {
+            this.collaterals.put(collateralType, amount);
+        }
+        else {
+            this.collaterals.replace(collateralType, this.collaterals.get(collateralType)+amount);
+        }
+        return this.collaterals;
+    }
 
+    public double getBsktHoldings() { return this.bsktHoldings; }
+    public void setBsktHoldings(double bsktHoldings) {this.bsktHoldings = bsktHoldings;}
+
+
+    public HashMap<String,Double> getFeesOwed() { return this.feesOwed; }
+    public void setFeesOwed(HashMap<String,Double> feesOwed) { this.feesOwed = feesOwed;}
+    // Add fees without conflict
+    public HashMap<String,Double> addFees(String feeType, double amount) {
+        if(!this.collaterals.containsKey(feeType)) {
+            this.collaterals.put(feeType, amount);
+        }
+        else {
+            this.collaterals.replace(feeType, this.collaterals.get(feeType)+amount);
+        }
+        return this.collaterals;
+    }
 }
