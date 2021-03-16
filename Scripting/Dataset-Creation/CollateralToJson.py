4fc0f0df2ea7026c3ab53f267a0a4c87e15f7d0b
SamirFarhat17 <samirfarhat17@gmail.com>
2021-03-16 17:48:02 +0000

    Removed redundant report directory

--- Scripting/Dataset-Creation/CollateralToJson.py
+++ Scripting/Dataset-Creation/CollateralToJson.py
@@ -7,23 +7,26 @@ import main
 
 
 data_path = "/../../Data/Oracle-Data/"
-wrappedBTC_data_path = os.path.join(os.getcwd() + data_path + 'W_BTC_GBP.csv')
+
+gbp_data_path = os.path.join(os.getcwd() + data_path + 'GBP-USD.csv')
+gbp = pd.read_csv(gbp_data_path)
+dates = gbp['Date'].values
+gbp_values = gbp['Open'].values
+
+wrappedBTC_data_path = os.path.join(os.getcwd() + data_path + 'W-BTC-USD.csv')
 wrappedBTC = pd.read_csv(wrappedBTC_data_path)
+btc_values = wrappedBTC['Open'].values
 
-dates = wrappedBTC['Date'].values
-values = wrappedBTC['Open'].values
-
-index = 0
+btc_index = 0
 oracle_BTC = {}
 for date in dates:
     oracle_BTC[date] = []
     oracle_BTC[date].append(
         {
-            'date': dates[index],
-            'Value': values[index]
+            'Value': btc_values[btc_index] * (1/gbp_values[btc_index])
         }
     )
-    index = index + 1
+    btc_index = btc_index + 1
 
 with open(os.getcwd() + data_path + "oracle_BTC.json", "w") as write_file:
     json.dump(oracle_BTC, write_file)
