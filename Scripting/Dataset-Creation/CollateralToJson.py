356c9af6bb4e3b05c4a0ec58f63a2db5f03b627b
SamirFarhat17 <samirfarhat17@gmail.com>
2021-03-16 20:56:45 +0000

    Oracle additions

--- Scripting/Dataset-Creation/CollateralToJson.py
+++ Scripting/Dataset-Creation/CollateralToJson.py
@@ -30,3 +30,39 @@ for date in dates:
 
 with open(os.getcwd() + data_path + "oracle_BTC.json", "w") as write_file:
     json.dump(oracle_BTC, write_file)
+
+eth_data_path = os.path.join(os.getcwd() + data_path + 'ETH-USD.csv')
+eth = pd.read_csv(eth_data_path)
+eth_values = eth['Open'].values
+
+eth_index = 0
+oracle_ETH = {}
+for date in dates:
+    oracle_ETH[date] = []
+    oracle_ETH[date].append(
+        {
+            'Value': eth_values[eth_index] * (1/gbp_values[eth_index])
+        }
+    )
+    eth_index = eth_index + 1
+
+with open(os.getcwd() + data_path + "oracle_ETH.json", "w") as write_file:
+    json.dump(oracle_ETH, write_file)
+
+ltc_data_path = os.path.join(os.getcwd() + data_path + 'P-LTC-USD.csv')
+ltc = pd.read_csv(ltc_data_path)
+ltc_values = ltc['Open'].values
+
+ltc_index = 0
+oracle_LTC = {}
+for date in dates:
+    oracle_LTC[date] = []
+    oracle_LTC[date].append(
+        {
+            'Value': ltc_values[ltc_index] * (1/gbp_values[ltc_index])
+        }
+    )
+    ltc_index = ltc_index + 1
+
+with open(os.getcwd() + data_path + "oracle_P_LTC.json", "w") as write_file:
+    json.dump(oracle_LTC, write_file)
